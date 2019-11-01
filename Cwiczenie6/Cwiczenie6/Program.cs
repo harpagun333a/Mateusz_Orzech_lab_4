56aa9a70201e325363f0a65cc4c5da0e62b8fa71
harpagun333a <matorz@student.agh.edu.pl>
2019-11-01 16:24:28 +0100

    Cwiczenie 6 done

---
+++ Cwiczenie6/Cwiczenie6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cwiczenie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 6");
+
+            string input = "";
+
+            while(input != "q")
+            {
+                Console.Write("\nIn: ");
+                input = Console.ReadLine();
+
+                for(int i = input.Length - 1; i > 0; i--)
+                {
+                    input = input.Insert(i, " ");
+                }
+
+                Console.WriteLine("Out: " + input);
+            }
+        }
+    }
+}
