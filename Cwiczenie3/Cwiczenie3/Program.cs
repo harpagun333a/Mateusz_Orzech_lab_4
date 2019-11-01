853a57eb543dfb453323ab2f5edd48f5d63a217d
harpagun333a <matorz@student.agh.edu.pl>
2019-11-01 13:41:39 +0100

    Cwiczenie 3 skonczone

---
+++ Cwiczenie3/Cwiczenie3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Cwiczenie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 3");
+            string input;
+
+            int tmp_int;
+            float tmp_float;
+            string tmp_string;
+
+            int inty = 0;
+            int floaty = 0;
+            int stringi = 0;
+
+            while((input = Console.ReadLine()) != "-1")
+            {
+                Console.WriteLine("Napisz daną typu int, float lub string:");
+                if (int.TryParse(input, out tmp_int))
+                {
+                    inty += 1;
+                }
+                else if (float.TryParse(input, out tmp_float))
+                {
+                    floaty += 1;
+                }
+                else
+                {
+                    stringi += 1;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Liczba wprowadzonych zmiennych typu int: " + inty.ToString());
+            Console.WriteLine("Liczba wprowadzonych zmiennych typu float: " + floaty.ToString());
+            Console.WriteLine("Liczba wprowadzonych zmiennych typu string: " + stringi.ToString());
+
+        }
+    }
+}
