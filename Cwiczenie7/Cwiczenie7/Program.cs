4a46c3511e24defbec935d95c440f2ff9a9fc8d8
harpagun333a <matorz@student.agh.edu.pl>
2019-11-01 17:30:35 +0100

    Cwiczenie 7 done

---
+++ Cwiczenie7/Cwiczenie7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cwiczenie7
+{
+    class Program
+    {
+        static int maxIntValue = 2147483647;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 7");
+
+            int n;
+
+            Console.WriteLine("\nPrzykład przepełnienia");
+            n = maxIntValue;
+            Console.WriteLine(n);
+            n += 10;
+            Console.WriteLine(n);
+            Console.WriteLine();
+
+            // Wyrzucenie błędu podczas przepełnienia
+            try
+            {
+                n = checked(maxIntValue + 10);
+            }
+            catch (System.OverflowException e)
+            {
+                Console.WriteLine("Wystąpił błąd przepełnienia.");
+            }
+
+        }
+    }
+}
