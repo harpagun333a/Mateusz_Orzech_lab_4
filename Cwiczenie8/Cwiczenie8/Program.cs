462d81261007ac49ec544c6287fd535b78724d77
harpagun333a <matorz@student.agh.edu.pl>
2019-11-02 16:21:12 +0100

    Cwiczenie8 and Cwiczenie9 done

---
+++ Cwiczenie8/Cwiczenie8/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Cwiczenie8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 8\n");
+
+            // punkt nr 1
+            Point p = new Point(1, 2);
+            Coords c = new Coords(3, 4);
+
+            p.print();
+            c.print();
+
+            Fun1(p);
+            Fun2(c);
+
+            Console.WriteLine();
+            p.print();
+            c.print();
+
+            // punkt nr 2
+            Point p1 = new Point(3, 3);
+            Point p2 = new Point(3, 3);
+            Coords c1 = new Coords(3, 3);
+            Coords c2 = new Coords(3, 3);
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(Object.Equals(p1, p2));
+            Console.WriteLine(Object.Equals(c1, c2));
+            Console.WriteLine(Object.Equals(c1, p1));
+
+            Console.WriteLine();
+
+            Console.WriteLine(p1.Equals(p2));
+            Console.WriteLine(c1.Equals(c2));
+            Console.WriteLine(c1.Equals(p1));
+            Console.WriteLine(p2.Equals(c2));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Object.ReferenceEquals(p1, p2));
+            Console.WriteLine(Object.ReferenceEquals(c1, c2));
+            Console.WriteLine(Object.ReferenceEquals(c1, p1));
+
+        }
+
+        public struct Coords
+        {
+            public int x, y;
+            public Coords(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+
+            public void print()
+            {
+                Console.WriteLine("Coords:\nx: " + x + "\ny: " + y);
+            }
+        }
+
+        public class Point
+        {
+            public int x, y;
+            public Point(int p1, int p2)
+            {
+                x = p1;
+                y = p2;
+            }
+
+            public void print()
+            {
+                Console.WriteLine("Punkt:\nx: " + x + "\ny: " + y);
+            }
+        }
+
+        static void Fun1(Point p)
+        {
+            p.x = 6;
+            p.y = 6;
+        }
+
+        static void Fun2(Coords c)
+        {
+            c.x = 7;
+            c.y = 7;
+        }
+    }
+}
---
+++ Cwiczenie9/Cwiczenie9/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Cwiczenie9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 9");
+            Console.WriteLine("\nWprowadź formułę:");
+
+            int count = 0;
+            string input;
+            bool isError = false;
+            int int_to_try;
+
+            while ((input = Console.ReadLine()) != "q")
+            {
+
+                string[] liczby = input.Split(new Char[] { '+', '-' });
+                foreach (string s in liczby)
+                {
+                    if(!int.TryParse(s, out int_to_try))
+                    {
+                        isError = true;
+                    }    
+                }
+
+                if(isError)
+                {
+                    isError = false;
+                    Console.WriteLine("Błędna formuła.");
+                    continue;
+                }
+
+                string[] dzialania = input.Split(new Char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+
+                string[] temporary = new string[dzialania.Length - 2];
+                for (int i = 1; i < (dzialania.Length - 1); i++)
+                {
+                    temporary[i - 1] = dzialania[i];
+                }
+
+                dzialania = temporary;
+
+                count = int.Parse(liczby[0]);
+
+                for(int i = 1; i < liczby.Length; i++)
+                {
+                    if(dzialania[i-1] == "+")
+                    {
+                        count += int.Parse(liczby[i]);
+                    }
+                    if(dzialania[i-1] == "-")
+                    {
+                        count -= int.Parse(liczby[i]);
+                    }
+                }
+
+                Console.WriteLine(count);
+                Console.WriteLine("\nWprowadź formułę:");
+            }
+        }
+    }
+}
