245f1ad3bbfedfea4970f7f5976185acedda1ab2
harpagun333a <matorz@student.agh.edu.pl>
2019-11-03 21:50:39 +0100

    Cwiczenie10 done

---
+++ Cwiczenie10/Cwiczenie10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cwiczenie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 10");
+
+            short sh = 1;
+            int i = 2;
+            long l = 3;
+            uint ui = 4;
+            ushort ush = 5;
+
+
+            // Przyklady konwersji niejawnej
+            i = sh;
+            l = i;
+            l = sh;
+            ui = ush;
+
+            // Przykłady konwersji jawnej
+            sh = (short)i;
+            i = (int)l;
+            sh = (short)l;
+            ush = (ushort)ui;
+        }
+    }
+}
